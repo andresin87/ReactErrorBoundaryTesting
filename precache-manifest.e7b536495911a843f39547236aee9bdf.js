3195c086e3187dd2203f7b5daf41cc846411fe0a
Andrés Lucas <alucas@redpoints.com>
2019-09-03 14:49:28 +0200

    Updates

---
+++ precache-manifest.e7b536495911a843f39547236aee9bdf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87293c50b88ac283f1ff2c34afbc23e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c85e74fa202723b6dbcd",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "ba479d58283595f9b39d",
+    "url": "/static/js/2.086a7d22.chunk.js"
+  },
+  {
+    "revision": "c85e74fa202723b6dbcd",
+    "url": "/static/js/main.6e8df2eb.chunk.js"
+  },
+  {
+    "revision": "eef23e4839988c9ea291",
+    "url": "/static/js/runtime~main.ec625690.js"
+  }
+]);
